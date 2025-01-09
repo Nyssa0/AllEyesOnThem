906888bdfa40fc53ec4dd1ed583ecff1b2806130
YesIDo49 <mehdialsidcheikh@gmail.com>
2025-01-10 00:14:17 +0100

    💫 add markers style and animation

--- frontend-next/src/app/components/Globe.jsx
+++ frontend-next/src/app/components/Globe.jsx
@@ -3,11 +3,9 @@ import styles from "../style/components/globe.scss";
 import { Canvas, useLoader, useFrame, extend } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import {useEffect, useRef, useState} from 'react';
-import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import { useThree } from '@react-three/fiber';
-
-// Étendre le renderer pour CSS2D
-extend({ CSS2DRenderer });
+import * as THREE from 'three';
+import { useGLTF, Html, OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 
 // Convertir latitude/longitude en coordonnées 3D
 function latLongToXYZ(lat, lon, radius = 1) {
@@ -20,75 +18,44 @@ function latLongToXYZ(lat, lon, radius = 1) {
 
     return [x, y, z];
 }
-function CSS2DRendererSetup() {
-    const { gl, scene, camera } = useThree();
 
-    useEffect(() => {
-        const labelRenderer = new CSS2DRenderer();
-        labelRenderer.setSize(window.innerWidth, window.innerHeight);
-        labelRenderer.domElement.style.position = 'absolute';
-        labelRenderer.domElement.style.top = '0px';
-        labelRenderer.domElement.style.pointerEvents = 'none'; // Pour éviter de bloquer l'interaction avec la scène
-        document.body.appendChild(labelRenderer.domElement);
+// Ajouter un marqueur
+function Marker({...props }) {
+    const ref = useRef();
+    const [isOccluded, setOccluded] = useState();
+    const [isInRange, setInRange] = useState();
+    const isVisible = isInRange && !isOccluded;
+    const vec = new THREE.Vector3();
 
-        const render = () => {
-            labelRenderer.render(scene, camera);
-        };
-
-        gl.setAnimationLoop(render);
-
-        return () => {
-            gl.setAnimationLoop(null);
-            document.body.removeChild(labelRenderer.domElement);
-        };
-    }, [gl, scene, camera]);
-
-    return null;
-}
-
-// Ajouter un marqueur CSS2D
-function Marker({ position, text }) {
-    const markerRef = useRef();
-
-    useEffect(() => {
-        if (!markerRef.current) return;
-
-        const div = document.createElement('div');
-        div.className = 'marker';
-        div.textContent = text;
-        div.style.color = 'white';
-        div.style.borderRadius = '4px';
-        div.style.padding = '5px';
-        div.style.fontSize = '12px';
-
-        const cssObject = new CSS2DObject(div);
-        cssObject.position.set(...position);
-
-        markerRef.current.add(cssObject);
-
-        return () => {
-            if (markerRef.current) {
-                markerRef.current.remove(cssObject);
-            }
-        };
-    }, [position, text]);
-
-    return <group ref={markerRef}></group>;
-}
-
-function Markers({ markers, radius = 1 }) {
+    useFrame((state) => {
+        const range = state.camera.position.distanceTo(ref.current.getWorldPosition(vec)) <= 10;
+        if (range !== isInRange) setInRange(range);
+    });
     return (
-        <>
-            {markers.map((marker, index) => {
-                const [x, y, z] = latLongToXYZ(marker.lat, marker.lon, radius); // Légèrement au-dessus du globe
-                return <Marker key={index} position={[x, y, z]} text={marker.label} />;
-            })}
-        </>
+        <group
+            ref={ref}
+        >
+            <Html
+                transform
+                occlude
+                onOcclude={setOccluded}
+                sprite
+                distanceFactor={10}
+                precision={0.1}
+                style={{
+                    transition: 'all 0.2s',
+                    opacity: isVisible ? 1 : 0,
+                    transform: `scale(${isVisible ? 1 : 0.25})`,
+                }}
+                {...props}
+            >
+            </Html>
+        </group>
+
     );
 }
 
-
-function RotatingGlobe() {
+function RotatingGlobe(props) {
     // Charger les textures
     const [color, normal, aoMap] = useLoader(TextureLoader, [
         './assets/color.jpg',
@@ -99,32 +66,25 @@ function RotatingGlobe() {
     // Référence pour le mesh du globe
     const globeRef = useRef();
     const [rotationSpeed, setRotationSpeed] = useState(0.003); // Vitesse de rotation
-    const [scale, setScale] = useState(2.5); // Échelle initiale
+    const [scale, setScale] = useState(2); // Échelle initiale
     const [isHovered, setIsHovered] = useState(false); // État hover
     const [isDragging, setIsDragging] = useState(false);
     const [lastMousePosition, setLastMousePosition] = useState({ x: 0, y: 0 }); // Dernière position de la souris
     const dragVelocity = useRef({ x: 0, y: 0 }); // Vitesse de rotation pendant le drag
     const dragDecay = 0.95; // Amortissement de la vitesse
-
-    // Données des markers (latitude, longitude)
-    const markers = [
-        { lat: 48.8566, lon: 2.3522 }, // Paris, France
-        { lat: 40.7128, lon: -74.006 }, // New York, USA
-        { lat: 35.6895, lon: 139.6917 }, // Tokyo, Japon
-        { lat: -33.8688, lon: 151.2093 }, // Sydney, Australie
-    ];
+    const [isMarkerHovered, setIsMarkerHovered] = useState(false); // État global pour le survol des marqueurs
 
     // Rotation à chaque frame
     useFrame(() => {
         if (globeRef.current) {
             // Interpolation pour ralentir ou accélérer en fonction de l'état `isHovered`
-            const targetSpeed = isHovered ? 0 : 0.003; // 0 pour arrêt, 0.003 pour vitesse normale
+            const targetSpeed = isHovered || isMarkerHovered ? 0 : 0.003; // 0 pour arrêt, 0.003 pour vitesse normale
             setRotationSpeed((speed) =>
                 Math.abs(speed - targetSpeed) < 0.0001 ? targetSpeed : speed + (targetSpeed - speed) * 0.03
             );
 
             // Interpolation pour l'échelle
-            const targetScale = isHovered ? 2.7 : 2.5;
+            const targetScale = isHovered || isMarkerHovered ? 2.2 : 2;
             setScale((currentScale) =>
                 Math.abs(currentScale - targetScale) < 0.001
                     ? targetScale
@@ -174,8 +134,10 @@ function RotatingGlobe() {
         setIsHovered(false);
     };
 
+    const palestineCoords = latLongToXYZ(31.9522, 35.2332, 1.01);
+    const congoCoords = latLongToXYZ(-1.6585, 19.0156, 1.01);
     return (
-        <>
+        <group {...props} dispose={null}>
             <mesh
                 ref={globeRef}
                 onPointerOver={() => setIsHovered(true)}
@@ -183,48 +145,35 @@ function RotatingGlobe() {
                 onPointerDown={handlePointerDown}
                 onPointerMove={handlePointerMove}
                 onPointerOut={handlePointerUp}
+                scale={0.15}
             >
                 <sphereGeometry args={[1, 64, 64]} />
                 <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
-                <Markers markers={markers} radius={1} />
+                <Marker position={palestineCoords}>
+                    <div className="marker"
+                         onPointerOver={() => setIsMarkerHovered(true)}
+                         onPointerOut={() => setIsMarkerHovered(false)}
+                    >
+                    </div>
+                </Marker>
+
+                <Marker position={congoCoords}>
+                    <div className="marker"
+                         onPointerOver={() => setIsMarkerHovered(true)}
+                         onPointerOut={() => setIsMarkerHovered(false)}
+                    >
+                    </div>
+                </Marker>
             </mesh>
-        </>
+
+        </group>
     );
 }
-
-function CSS2DRendererWrapper() {
-    const { gl, scene, camera } = useThree();
-    const rendererRef = useRef();
-
-    useEffect(() => {
-        const renderer = new CSS2DRenderer();
-        renderer.domElement.style.position = 'absolute';
-        renderer.domElement.style.top = '0';
-        renderer.domElement.style.pointerEvents = 'none';
-        renderer.domElement.style.zIndex = '10';
-        document.body.appendChild(renderer.domElement);
-        rendererRef.current = renderer;
-
-        return () => {
-            document.body.removeChild(renderer.domElement);
-        };
-    }, []);
-
-    useFrame(() => {
-        if (rendererRef.current) {
-            rendererRef.current.render(scene, camera);
-        }
-    });
-
-    return null;
-}
-
 
 export default function Globe() {
     return (
         <Canvas>
-            <CSS2DRendererSetup />
-            <ambientLight intensity={5} />
+            <ambientLight intensity={15} />
             <RotatingGlobe />
         </Canvas>
     );
